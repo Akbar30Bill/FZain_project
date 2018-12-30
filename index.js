38a31162edc60b402b55623c186d3fbae8ea2718
Akbar30Bill <alishahverdi2010@yahoo.com>
2018-12-30 22:48:47 +0330

    iluay

--- index.js
+++ index.js
@@ -9,18 +9,27 @@ var url = 'mongodb://user:1qw23e@ds143474.mlab.com:43474/fzain'
 mongoose.connect(url);
 mongoose.Promise = global.Promise;
 var user = new mongoose.Schema({
-  username:{type:String , require:true},
+  username:{type:String , require:true , unique:true},
   password:{type:String , require:true},
-  email:{type:String , require:true},
+  email:{type:String , require:true , unique:true},
   names:{type:String , require:true},
   gp_name:{type:String , require:true},
   frame:{type:String , require:true}
 });
 var user_schema = mongoose.model('user_schema', user);
-user.auth = function(email , password)
+user.auth = function(gp_name_ , password_)
 {
-
-}
+  console.log(this);
+  usr = this.where({gp_name: new RegExp(gp_name_ , 'i')});
+  if (usr.password == sha512(password_))
+  {
+    return usr.frame;
+  }
+  else
+  {
+    return 'incorrect password';
+  }
+};
 // const mongo = require('mongodb').MongoClient
 // const url = 'mongodb://akbar:Akbar30bill@ds143474.mlab.com:43474/fzain'
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -40,19 +49,20 @@ app.use(bodyParser.urlencoded());     // to support URL-encoded bodies
 app.get('/', function(req , res){
   console.log('presented root page to: ');
   console.log(req.ip);
-  res.sendFile(path.join(__dirname + '/root.html'));
+  res.sendFile(path.join(__dirname + '/login.html'));
 });
 app.get('/signup', function(req , res){
   console.log('presented signup page to: ');
   console.log(req.ip);
   res.sendFile(path.join(__dirname + '/Signup2.html'));
 });
-app.post('/' , function(req,res){
-  var username = req.body.username;
-  var password = req.body.password
-  password = sha512(password);
-  console.log("username: " + username);
-  console.log("password: " + password);
+app.post('/login' , function(req,res){
+  var gp_name_ = req.body.groupname;
+  var password_ = req.body.password;
+  console.log("username: " + gp_name_);
+  console.log("password: " + password_ );
+  result = user_schema.find({"gp_name":gp_name_});
+  console.log(result);
 });
 
 app.post('/signup' , function(req,res){
