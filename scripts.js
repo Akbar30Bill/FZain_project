e015cfdf877eb32bd6bb8d8f9aefa1ae61455d15
nmgtav <nima.mg@gmail.com>
2018-12-30 16:48:29 +0330

    Remove Member button debugged

--- scripts.js
+++ scripts.js
@@ -3,20 +3,17 @@ let memCount = 1;
 function addMemberField () {
     memCount++;
     let fs = document.getElementById("members");
-    let btns = document.getElementById("buttons").cloneNode(true);
+    let buttons = document.getElementById("buttons");
+    let btns = buttons.cloneNode(true);
     let inp = document.createElement("input");
-    inp.type="text";
-    inp.name="member " + memCount;
+    buttons.parentNode.removeChild(buttons);
+    inp.type = "text";
+    inp.name = "member " + memCount;
     inp.placeholder = "Member " + memCount;
-    inp.className ="field";
-    if (memCount == 2)
-        for (let i = 0 ; i < 3; i++) fs.removeChild(fs.lastChild);
-    else
-        for (let i = 0 ; i < 2; i++) fs.removeChild(fs.lastChild);
+    inp.className = "field";
     fs.appendChild(inp);
     fs.appendChild(document.createElement("br"));
     fs.appendChild(btns);
-    fs.appendChild(document.createElement("br"));
 }
 
 function removeField () {
@@ -28,11 +25,7 @@ function removeField () {
     let y = x.cloneNode(true);
     let fs = document.getElementById("members");
     x.parentNode.removeChild(x);
-    let n = 0;
-    if (memCount == 2) n = 2;
-    else n = 3;
-    console.log(n);
-    for (let i = 0 ; i < n; i++) {
+    for (let i = 0 ; i < 2; i++) {
         console.log(fs.lastChild);
         fs.removeChild(fs.lastChild);
     }
