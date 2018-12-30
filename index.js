cd147b02235d6b73236e810970aa52df6d0f4e9b
Akbar30Bill <alishahverdi2010@yahoo.com>
2018-12-30 21:05:57 +0330

    now i am fucked up

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const port = 3000;
 var path = require('path');
 var bodyParser = require('body-parser')
 var sha512 = require('js-sha512');
-const mongo = require('mongodb').MongoClient
-const url = 'mongodb://akbar:Akbar30bill@ds143474.mlab.com:43474/fzain'
+var mongoose = require('mongoose');
+var schema = mongoose.Schema({
+  username:String,
+  password:String,
+  email:String,
+  names:String,
+  gp_name:String,
+  frame:String
+});
+// const mongo = require('mongodb').MongoClient
+// const url = 'mongodb://akbar:Akbar30bill@ds143474.mlab.com:43474/fzain'
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -42,22 +51,18 @@ app.post('/' , function(req,res){
 
 app.post('/signup' , function(req,res){
   console.log('Signing up');
-  var name = req.body.name;
-  var username = req.body.username;
-  var password = req.body.password;
-  password = sha512(password);
-  console.log("name: " + name);
-  console.log("username: " + username);
-  console.log("password: " + password);
-  mongo.connect(url, (err, client) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    else{
-      client.createUser(username , password);
-    }
-  });
+  var names_ = req.body.name;
+  var password_ = req.body.password;
+  var email_ = req.body.email;
+  var gp_name_ = req.body.group_name;
+  var username_ = req.body.username;
+  var frame_ = req.body.frame;
+  password_ = sha512(password_);
+  console.log("name: " + names_);
+  console.log("username: " + username_);
+  console.log("password: " + password_);
+
 });
+var username_ = req.body.username;
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
