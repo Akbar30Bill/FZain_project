3844a41271e042bf5acff87fb999668f5b02285c
akbar30bill <alishahverdi2010@yahoo.com>
2019-01-02 19:22:38 +0330

    port changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8000;
 var path = require('path');
 var bodyParser = require('body-parser')
 var sha512 = require('js-sha512');
