d15b09584d96d650243ca334e9e297edf31c51ea
akbar30bill <alishahverdi2010@yahoo.com>
2019-01-02 19:32:57 +0330

    auto push using push script

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+app.listen(port, () => console.log(`app listening on port ${port}!`));
 var path = require('path');
 var bodyParser = require('body-parser')
 var sha512 = require('js-sha512');
